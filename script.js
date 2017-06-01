70065d1aa6cb2618229dd5ad2696895025839952
Kirill Bubentsov <kirill.dm6@gmail.com>
2017-06-01 21:42:20 +0300

    3

--- script.js
+++ script.js
@@ -122,8 +122,9 @@
 // }
 // returnNoun(555,arr); ///555 яблок
 
+
 //7
-//все счасливые билеты 6 значные символы
+//все счастливые билеты с 6 значными номерами
 //функция для преобразования числа к нужному формату
 // function getZero(number){
 //     var length = number.toString().length;
@@ -146,8 +147,9 @@
 //         return number.toString()
 //     }
 // }
-//цикл для сверки значений
+// цикл для сверки значений
 // var number = 0;
+// var result = [];
 // while(number <= 999999){
 //     number++;
 //     var arr = getZero(number).slice('');
@@ -164,17 +166,214 @@
 //     var resultLeft = 0;
 //     var resultRight = 0;
 //     for(var i = 0; i < numberLeft.length; i++){
-//         resultLeft +=numberLeft[i]
+//         resultLeft += numberLeft[i]
 //     }
 //     for(var i = 0; i < numberRight.length; i++){
-//         resultRight +=numberRight[i]
+//         resultRight += numberRight[i]
 //     }
 //     if(resultLeft == resultRight){
-//         console.log(getZero(number))
+//         result.push(getZero(number))
 //     }
 // }
+// console.log(result);
 
 
 //8
+//перемешать массив в случайном порядке
+// var arr = [1,2,3,4,5,6,7,8,9,10];
+// function shuffle(arr){
+//     arr.sort(function(a,b){
+//         return Math.random() - 0.5;
+//     });
+//     console.log(arr)
+// }
+// shuffle(arr);
+
+
+//9
+//перемещать символы строки в случайном порядке
+// var str = 'lorem ipsum dolores';
+// function strShuffle(str){
+//     var arr = str.split('');
+//     arr.sort(function(a,b){
+//         return Math.random() - 0.5
+//     })
+//     var str = arr.join('');
+//     console.log(str);
+// }
+// strShuffle(str);
+
+
+//10
+//удалить дубли из массива
+// var arr = [1,2,3,4,5,5,6,6,7,8,9,10];
+// function union(arr){
+//     var result = [];
+//     NextInput:
+//     for(var i = 0; i < arr.length; i++){
+//         var val = arr[i];
+//         for(var j = 0; j < result.length; j++){
+//             if(result[j] == val){
+//                 continue NextInput;
+//             }
+//         }
+//         result.push(val)
+//     }
+//     return result;
+// }
+// console.log(union(arr));
+
+//11
+// вернуть элементы встречающиеся в каждом массиве - ??????? xз как сделать
+// var arr1 = [1, 2, 3];
+// var arr2 = [101, 2, 1, 10];
+// var arr3 = [2,1];
+// function union(arr1,arr2,arr3) {
+//     var result = [];
+//     var arr = [];
+//     for(var i = 0; i < arguments.length; i++){
+//         var array = arguments[i];
+//         for(var j = 0; j < array.length; j++){
+//            var elem = array[i]
+//            if(array.indexOf(elem) !== -1){
+//                arr.push(elem)
+//            }
+//         }
+//     }
+//     NextInput:
+//     for(var z = 0; z < arr.length; z++){
+//         var promElem = arr[z];
+//         for(var q = 0; q < result.length; q++){
+//             if(result[q] !== promElem) continue NextInput;
+//             result.push(promElem);
+//         }
+//     }
+//     return result;
+// }
+// console.log(union(arr1,arr2,arr3));
+
+//12
+//вывести определенное количество первых символов строки
+// var str = 'lorem ipsun';
+// function cut(str, number){
+//     if(number == undefined){
+//         number = 10;
+//     }
+//     var arr = str.split('');
+//     for(var i = 0; i < number; i++){
+//         arr.unshift(arr[0]);
+//     }
+//     var str2 = arr.join('');
+//     return str2;
+// }
+// console.log(cut(str,12));
+
+//13
+//среднее арефметическое елементов массива
+// var arr = [1,2,3,4,5,6,7,8,9,10];
+// function getSumArifmetic(arr){
+//     var result = 0;
+//     for(var i = 0; i < arr.length; i++){
+//         result += arr[i];
+//     }
+//     return result/i;
+// }
+// console.log(getSumArifmetic(arr));
+
+
+//******************************************DOM**********************************
+//1
+//поменять текс тега по нажатию на клавишу
+// var button = document.querySelector('button');
+// var span = document.querySelector('span');
+// button.addEventListener('click',function(){
+//     span.innerHTML = '!!!';
+// });
+
+
+//2
+//менять span на b не изменяя текста
+// var button = document.querySelector('button');
+// var span = document.querySelector('span');
+// button.addEventListener('click', function(){
+//     var text = span.textContent;
+//     span.remove();
+//     var b = document.createElement('b');
+//     b.innerHTML = text;
+//     document.body.appendChild(b);
+// })
+
+
+//3
+//заменить текс обзацев на index в соответствии с вызовом
+// var button = document.querySelector('button');
+// var elems = document.querySelectorAll('p');
+// console.log(elems);
+// button.addEventListener('click', function(){
+//     for(var i = 0; i < elems.length; i++){
+//         elems[i].innerHTML = i + 1;
+//     }
+// });
+
+
+//4
+//вывести значения input
+// var button = document.querySelector('button');
+// var input = document.querySelector('input');
+// button.addEventListener('click', function () {
+//     var text = input.value;
+//     result.innerHTML = text;
+//     input.value = '';
+// });
+
+
+//5
+//содержимое input возвести в квадрат
+// var button = document.querySelector('button');
+// var input = document.querySelector('input');
+// button.addEventListener('click', function(){
+//    var value = +input.value;
+//    result.innerHTML = value * value;
+// });
+
+
+//6
+//поменяться значениями в input
+// var button = document.querySelector('button');
+// var input1 = document.getElementById('input1');
+// var input2 = document.getElementById('input2');
+// button.addEventListener('click', function(){
+//     var text1 = input1.value;
+//     var text2 = input2.value;
+//     input1.value = text2;
+//     input2.value = text1;
+// });
+
+//7
+//изменить текст на кнопке
+// var button = document.querySelector('input');
+// button.addEventListener('click', function(){
+//     button.value = 'some text'
+// });
+
+//8
+//поменять цвет текста в input
+// var button = document.querySelector('button');
+// var input = document.querySelector('input');
+// button.addEventListener('click', function(){
+//     input.style.color  = 'red';
+// });
+
+//9
+//заблокировать разблокировать ввод в input
+// var block = document.querySelectorAll('button')[0];
+// var unblock = document.querySelectorAll('button')[1];
+// var input = document.querySelector('input');
+// block.addEventListener('click', function(){
+//     input.disabled = true;
+// })
+// unblock.addEventListener('click', function(){
+//     input.disabled = false;
+// })
 
 
