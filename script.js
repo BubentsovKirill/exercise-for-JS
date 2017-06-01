8c08c531eca222a470dfc162a995942cd4537505
Kirill Bubentsov <kirill.dm6@gmail.com>
2017-06-01 12:27:00 +0300

    1

---
+++ script.js
@@ -0,0 +1,28 @@
+//*******************МАССИВЫ*****************************//
+//разбиение массива на подмассивы
+// var arr =  [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// function dividerArr(arr, divider){
+//     var newArr = [];
+//     for(var i = 0; i < arr.length; i++){
+//         if(i % divider == 0){
+//             newArr.push([]);
+//         }
+//         newArr[newArr.length-1].push(arr[i]);
+//     }
+//     return newArr
+// }
+// console.log(dividerArr(arr, 4));
+
+//проверка на наличие класса в строке и добавление его если нету
+// var str = 'menu open close';
+// function addClass(str, cls){
+//     var arr = str.split(' ');
+//     for(var i = 0; i < arr.length; i++){
+//         if(arr[i] === cls) return;
+//     }
+//     arr.push(cls);
+//     var str = arr.join(' ');
+//     return str
+// }
+// console.log(addClass(str, 'get'));
+// console.log(addClass(str, 'get'));
